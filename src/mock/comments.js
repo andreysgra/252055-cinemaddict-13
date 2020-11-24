efb732c8034849398b738fd18004974f29f0ef53
Andrey Grachev <andreysgra@gmail.com>
2020-11-24 21:05:46 +0300

    Добавляет дату в структуру комментария к фильму.

--- src/mock/comments.js
+++ src/mock/comments.js
@@ -1,8 +1,7 @@
-import {getRandomItem, setCounter} from "../utils/common";
 import Utils from '../utils';
 import * as consts from "./const";
 
-const commentId = setCounter(1000);
+const commentId = Utils.setCounter(1000);
 
 const generateCommentText = (sentences) => {
   const MIN_SENTENCES = 1;
@@ -11,12 +10,23 @@ const generateCommentText = (sentences) => {
   return Utils.generateItems(sentences, MIN_SENTENCES, MAX_SENTENCES).join(` `);
 };
 
+const generateDate = (year) => {
+  const MILLISECONDS_IN_DAY = 24 * 3600 * 1000;
+  const DAYS_IN_YEAR = 365;
+  const timestamp = MILLISECONDS_IN_DAY * DAYS_IN_YEAR * year;
+
+  return new Date(Date.now() - Utils.getRandomInteger(0, timestamp));
+};
+
 const generateComment = () => {
+  const MAX_YEAR = 3;
+
   return {
     id: commentId(),
     comment: generateCommentText(consts.sentences),
-    emotion: getRandomItem(consts.emotions),
-    author: getRandomItem(consts.filmActors),
+    emotion: Utils.getRandomItem(consts.emotions),
+    author: Utils.getRandomItem(consts.filmActors),
+    date: generateDate(MAX_YEAR)
   };
 };
 
