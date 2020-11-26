a3fb014bf377d50adccfe7e4443ba89042cdac51
Andrey Grachev <andreysgra@gmail.com>
2020-11-26 15:15:00 +0300

    Описывает функции создания и рендеринга элемента.

--- src/const.js
+++ src/const.js
@@ -2,3 +2,9 @@ export const FILMS_COUNT_PER_STEP = 5;
 export const FILMS_EXTRA_COUNT = 2;
 export const DESCRIPTION_LENGTH = 140;
 export const extraListTitles = [`Top rated`, `Most commented`];
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTEREND: `afterend`
+};
--- src/utils/render.js
+++ src/utils/render.js
@@ -1,4 +1,28 @@
+import {RenderPosition} from '../const';
+
 export default class Render {
+  static createElement(template) {
+    const element = document.createElement(`div`);
+
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  static renderElement(container, element, position = RenderPosition.BEFOREEND) {
+    switch (position) {
+      case RenderPosition.AFTERBEGIN:
+        container.prepend(element.getElement());
+        break;
+      case RenderPosition.BEFOREEND:
+        container.append(element.getElement());
+        break;
+      case RenderPosition.AFTEREND:
+        container.parentNode.insertBefore(element.getElement(), container.nextSibling);
+        break;
+    }
+  }
+
   static renderTemplate(container, template, position = `beforeend`) {
     return container.insertAdjacentHTML(position, template);
   }
