40dd6d9a40bb7c16ce983753f754b17757864948
Andrey Grachev <andreysgra@gmail.com>
2020-12-03 16:31:36 +0300

    Производит рефакторинг кода модуля комментариев.

--- src/view/film-comments.js
+++ src/view/film-comments.js
@@ -1,35 +1,33 @@
 import AbstractView from './abstract';
 import {FormatTime} from '../utils';
 
-const createCommentsTemplate = (commentIds, comments) => {
-  return commentIds
-    .map((commentId) => comments.find((comment) => comment.id === commentId))
-    .sort((a, b) => b.date - a.date)
-    .map((item) => {
-      const {comment, emotion, author, date} = item;
-      const commentDate = FormatTime.fullDateWithTime(date);
+const createCommentTemplate = (item) => {
+  const {comment, emotion, author, date} = item;
+  const commentDate = FormatTime.fullDateWithTime(date);
 
-      return `
-        <li class="film-details__comment">
-          <span class="film-details__comment-emoji">
-            <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-smile">
-          </span>
-          <div>
-            <p class="film-details__comment-text">${comment}</p>
-            <p class="film-details__comment-info">
-              <span class="film-details__comment-author">${author}</span>
-              <span class="film-details__comment-day">${commentDate}</span>
-              <button class="film-details__comment-delete">Delete</button>
-            </p>
-          </div>
-        </li>
-      `;
-    })
-    .join(``);
+  return `
+    <li class="film-details__comment">
+      <span class="film-details__comment-emoji">
+        <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-smile">
+      </span>
+      <div>
+        <p class="film-details__comment-text">${comment}</p>
+        <p class="film-details__comment-info">
+          <span class="film-details__comment-author">${author}</span>
+          <span class="film-details__comment-day">${commentDate}</span>
+          <button class="film-details__comment-delete">Delete</button>
+        </p>
+      </div>
+    </li>
+  `;
 };
 
 const createFilmCommentsTemplate = (commentIds, comments) => {
-  const commentsList = createCommentsTemplate(commentIds, comments);
+  const commentsList = commentIds
+    .map((commentId) => comments.find((comment) => comment.id === commentId))
+    .sort((a, b) => b.date - a.date)
+    .map((item) => createCommentTemplate(item))
+    .join(``);
 
   return `
     <ul class="film-details__comments-list">
