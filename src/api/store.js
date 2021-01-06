a2b901617c451fe2cc5525e4679b4d6dd6ea229f
Andrey Grachev <andreysgra@gmail.com>
2021-01-06 18:56:49 +0300

    Реализует хранилище.

---
+++ src/api/store.js
@@ -0,0 +1,43 @@
+export default class Store {
+  constructor(storeKey, storage) {
+    this._storage = storage;
+    this._storeKey = storeKey;
+  }
+
+  getItems() {
+    try {
+      return JSON.parse(this._storage.getItem(this._storeKey)) || {};
+    } catch (err) {
+      return {};
+    }
+  }
+
+  removeItem(key) {
+    const store = this.getItems();
+
+    delete store[key];
+
+    this._storage.setItem(this._storeKey, JSON.stringify(store));
+  }
+
+  setItem(key, value) {
+    const store = this.getItems();
+
+    this._storage.setItem(
+        this._storeKey,
+        JSON.stringify(
+            Object.assign(
+                {},
+                store,
+                {
+                  [key]: value
+                }
+            )
+        )
+    );
+  }
+
+  setItems(items) {
+    this._storage.setItem(this._storeKey, JSON.stringify(items));
+  }
+}
